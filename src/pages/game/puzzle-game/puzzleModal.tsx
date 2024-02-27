1f6cd910a66076717f17720a309a753a07846ef1
Jati Suryo Buwono <jati.buwono@ist.id>
2024-02-27 17:10:45 +0700

    feat : changed color

--- src/pages/game/puzzle-game/puzzleModal.tsx
+++ src/pages/game/puzzle-game/puzzleModal.tsx
@@ -7,12 +7,12 @@ interface ModalProps {
 const PuzzleModal: React.FC<ModalProps> = ({ onClose }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className="bg-green-400 p-8 rounded-lg">
+      <div className="bg-pink-400 p-8 rounded-lg">
         <h2 className="text-2xl font-bold mb-4">Congratulations!</h2>
         <p>You solved the puzzle!</p>
         <button
           onClick={onClose}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+          className="bg-purple-500 text-white px-4 py-2 rounded-md mt-4"
         >
           Close
         </button>
