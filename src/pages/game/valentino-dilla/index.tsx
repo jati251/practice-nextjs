289eefaae37b9859b14fb2b0f37e285eaa710b99
Jati Suryo Buwono <jati.buwono@ist.id>
2024-02-27 09:45:47 +0700

    feat : added valentino dilla

---
+++ src/pages/game/valentino-dilla/index.tsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const loveMessages = [
+  "You make my heart skip a beat.",
+  "I love you more than words can express.",
+  "You are the light of my life.",
+  "You mean everything to me.",
+  "My love for you grows stronger every day.",
+  "You are my soulmate.",
+  "I am grateful for your love.",
+  "You are the best thing that ever happened to me.",
+  "I can't imagine my life without you.",
+  "You complete me.",
+  "I cherish every moment with you.",
+];
+const noMessage = [
+  "No",
+  "you sure ?",
+  "are you really sure ?",
+  "think again",
+  "last change",
+  "come on  :(",
+  "im sad , please dont",
+  "how dare you",
+  "i hate you",
+];
+
+const getRandomMessage = () => {
+  const randomIndex = Math.floor(Math.random() * loveMessages.length);
+  return loveMessages[randomIndex];
+};
+
+const ValentinePage = () => {
+  const [message, setMessage] = useState("");
+  const [title, setTitle] = useState("Will you be my Valentine?");
+  const [play, setPlay] = useState(false);
+  const [click, setclick] = useState(0);
+  const [next, isnext] = useState(false);
+  const [noClicks, setNoClicks] = useState(0);
+  const [showHearts, setShowHearts] = useState(false);
+
+  const handleYesClick = () => {
+    setShowHearts(true);
+    setTitle('Yeayy, thank you so much :"""DDD');
+  };
+
+  const generateMessage = () => {
+    const randomMessage = getRandomMessage();
+    setMessage(randomMessage);
+    setPlay(true);
+    setclick(click + 1);
+  };
+
+  const handleNoClick = () => {
+    if (noClicks < noMessage.length) {
+      setNoClicks(noClicks + 1);
+    }
+  };
+
+  useEffect(() => {
+    const audioElement = document.getElementById(
+      "audioPlayer"
+    ) as HTMLAudioElement;
+    if (audioElement && play) {
+      audioElement.play();
+    }
+  }, [play]);
+
+  useEffect(() => {
+    if (click > 5) {
+      isnext(true);
+    }
+  }, [click]);
+
+  return (
+    <div className="bg-pink-400 min-h-screen flex flex-col items-center justify-center">
+      <img src="/dilla.png" className="w-64 h-auto" alt="logo" />
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">{`Happy Valentine's Day!`}</h1>
+        {next ? (
+          <div>
+            <p className="text-lg mb-8">{title}</p>
+            <div className="flex justify-center gap-4">
+              <button
+                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+                style={{ fontSize: 16 + noClicks * 20 }}
+                onClick={handleYesClick}
+              >
+                Yes
+              </button>
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+                style={{ width: 100, height: "100%" }}
+                onClick={handleNoClick}
+              >
+                {noMessage[noClicks]}{" "}
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p className="text-lg mb-8">{`Welcome to our Valentine's Day page Dilla. Click the button below to generate a love message!`}</p>
+            <button
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+              onClick={generateMessage}
+            >
+              Generate Love Message
+            </button>
+            <p className="text-lg mt-4 animate-bounce">{message}</p>
+          </div>
+        )}
+      </div>
+      {showHearts && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <img src="./love.png" className="w-28 h-w-28 animate-spin" />
+          <img src="./love.png" className="w-28 h-w-28 animate-spin" />
+          <img src="./love.png" className="w-28 h-w-28 animate-spin" />
+        </div>
+      )}
+      <audio id="audioPlayer" autoPlay>
+        <source src="/audio.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
+};
+
+export default ValentinePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,46 @@
 // pages/Menu.tsx
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Menu: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [correct, isCorrect] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Here you can handle the submission of the input value
+    if (value === "november") {
+      isCorrect(true);
+    }
+    // Optionally, you can clear the input value after submission
+    setValue("");
+  };
+
   return (
     <div className="bg-gradient-to-br from-purple-500 to-pink-500 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg">
+        <div className="flex items-center justify-center h-screen">
+          <form onSubmit={handleSubmit} className="flex">
+            <input
+              type="text"
+              value={value}
+              onChange={handleChange}
+              className="appearance-none border border-gray-300 rounded-l-lg py-2 px-4 focus:outline-none focus:border-blue-500"
+              placeholder="Enter something Gen Z..."
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-r-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
         <h1 className="text-3xl font-bold text-center mb-8">Choose a Game:</h1>
         <ul className="flex flex-col items-center">
           <li className="mb-4">
@@ -37,6 +71,15 @@ const Menu: React.FC = () => {
               </div>
             </Link>
           </li>
+          {correct && (
+            <li className="mb-4">
+              <Link href="/game/snake-game" passHref>
+                <div className="block bg-pink-400 text-green-700 p-6 rounded-md text-center hover:bg-purple-700 transition-colors duration-300 cursor-pointer">
+                  Valentino Dilla
+                </div>
+              </Link>
+            </li>
+          )}
           {/* Add more games here */}
         </ul>
       </div>
